ae6e3058fc5a661e451d4f874917d675c727ca7e
Sadanand Sonkar <sadanand949@gmail.com>
2019-01-05 19:10:48 +0530

    Home assignment 1
    Added new Home Assignment

---
+++ config.js
@@ -0,0 +1,30 @@
+/*
+ * Create and Export Configuration variables
+ *
+ */
+
+// Contaniner for all the environments
+var environments = {};
+
+// Staging environment
+environments.staging = {
+	'httpPort': 3000,
+	'httpsPort': 3001,
+	'envName' : 'staging'
+};
+
+// Production environment
+environments.production = {
+	'httpPort': 5000,
+	'httpsPort': 5001,
+	'envName' : 'production'
+};
+
+// determine which environment was passed a command-
+var currentEnvironment  = typeof(process.env.NODE_ENV) == 'String' ? process.env.NODE_ENV.toLowerCase() : '';
+
+//check the current environment is one of the environments above,
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
